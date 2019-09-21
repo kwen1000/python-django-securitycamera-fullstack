c6507abb6bfe80f233456f3132721f0f0e886ea9
Kevin Wen <preferredcustomer@192.168.1.11>
2019-09-21 03:29:10 -0700

    Added post.html

--- security_camera/uploads/views.py
+++ security_camera/uploads/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Post
+
+from .forms import PostForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 class HomePageView(ListView):
     model = Post
     template_name = "home.html"
+
+class CreatePostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "post.html"
+    success_url = reverse_lazy("home")
